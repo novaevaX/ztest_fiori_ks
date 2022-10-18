2d629150f49e66bd20f29156ce66ef290353156f
novaevaX <74986906+novaevaX@users.noreply.github.com>
2022-10-18 16:28:44 +0300

    f1

---
+++ webapp/controller/Create01.controller.js
@@ -0,0 +1,132 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/UIComponent",
+	"sap/ui/core/routing/History",
+	"sap/ui/core/Fragment",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, History, UIComponent, JSONModel, Fragment, Filter, FilterOperator){
+    "use strict";
+    return Controller.extend("ztest_fiori_ks.controller.Create01", {
+
+		onInit: function () {
+			/*
+			console.log("Start load");
+			var oModel = this.getOwnerComponent().getModel("typeDoc"); 
+			//this.setModel(this.getComponentModel(sPath), "typeDoc");
+			//var oModel = new JSONModel(sPath);
+			console.log(oModel);
+			console.log("create oModel");
+			this.getView().setModel(oModel);
+			*/
+		},
+		onChange: function(oEvent) {
+			var oInput = oEvent.getSource();
+			this._validateInput(oInput);
+		},
+		_validateInput: function(oInput){
+			var bValidationError = false;
+			if(oInput === undefined){
+				bValidationError = true;
+			} else {
+				if(oInput._lastValue.length < oInput.mProperties.maxLength){
+					var sValueState = "Error";
+					bValidationError = true;
+				} else {
+					var sValueState = "Success";
+				} 
+				oInput.setValueState(sValueState);
+			}
+			return bValidationError;
+		},
+		onCreate: function(){
+			// collect input controls
+			var oView = this.getView(),
+				aInputs = [
+				oView.byId("typeInput"),
+				oView.byId("numberInput")
+			],
+				bValidationError = false;
+
+			// Check that inputs are not empty.
+			// Validation does not happen during data binding as this is only triggered by user actions.
+			aInputs.forEach(function (oInput) {
+				bValidationError = this._validateInput(oInput) || bValidationError;
+			}, this);
+
+			if (!bValidationError) {
+				var typeInput = oView.byId("typeInput")._lastValue;
+				sap.ui.getCore().setModel(typeInput, "typeInput");
+
+				var randInput = oView.byId("numberInput")._lastValue;
+				sap.ui.getCore().setModel(randInput, "randInput");
+
+				this.getOwnerComponent().getRouter().navTo("page3");
+			} else {
+				alert(this.getView().getModel("i18n").getResourceBundle().getText("msgErrorInput"));
+			}	
+		},
+
+        onBack : function () {
+			/*var sPreviousHasha = History.getInstance().getPreviousHash();
+			//The history contains a previous entry
+			console.log(sPreviousHasha);
+			if (sPreviousHash !== undefined) { */
+			if (window.history.length !== 1){
+				window.history.go(-1);
+			  } else {
+				this.getOwnerComponent().getRouter().navTo("mainPage");
+			  }
+			/* } else {
+				// There is no history!
+				// replace the current hash with page 1 (will not add an history entry)
+				this.getOwnerComponent().getRouter().navTo("page1", null, true);
+			} */
+		},
+
+		onExit: function(){
+			this.getOwnerComponent().getRouter().navTo("page1");
+		} /*,
+		onValueHelpRequest: function (oEvent) {
+			var sInputValue = oEvent.getSource().getValue(),
+				oView = this.getView();
+
+			if (!this._pValueHelpDialog) {
+				this._pValueHelpDialog = Fragment.load({
+					id: oView.getId(),
+					name: "sap.m.sample.InputAssisted.ValueHelpDialog",
+					controller: this
+				}).then(function (oDialog) {
+					oView.addDependent(oDialog);
+					return oDialog;
+				});
+			}
+			this._pValueHelpDialog.then(function(oDialog) {
+				// Create a filter for the binding
+				oDialog.getBinding("items").filter([new Filter("Name", FilterOperator.Contains, sInputValue)]);
+				// Open ValueHelpDialog filtered by the input's value
+				oDialog.open(sInputValue);
+			});
+		},
+
+		onValueHelpSearch: function (oEvent) {
+			var sValue = oEvent.getParameter("value");
+			var oFilter = new Filter("Name", FilterOperator.Contains, sValue);
+
+			oEvent.getSource().getBinding("items").filter([oFilter]);
+		},
+
+		onValueHelpClose: function (oEvent) {
+			var oSelectedItem = oEvent.getParameter("selectedItem");
+			oEvent.getSource().getBinding("items").filter([]);
+
+			if (!oSelectedItem) {
+				return;
+			}
+
+			this.byId("productInput").setValue(oSelectedItem.getTitle());
+		}
+		*/
+    });
+});
---
+++ webapp/controller/Create02.controller.js
@@ -0,0 +1,85 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
+	"sap/base/util/uid"
+], function (Controller, History, uid){
+    "use strict";
+	var date;
+	var state;
+	var id;
+    return Controller.extend("ztest_fiori_ks.controller.Create02", {
+		onInit : function (){
+			this._getDateResorces();
+			const myUniqueID = uid();
+			id = myUniqueID;
+			var newDate = new Date();
+			date = newDate;
+
+			this._getDateResorces();
+		},
+		_getDateResorces: function(){
+			const typeInput = sap.ui.getCore().getModel("typeInput");
+
+			const randInput = sap.ui.getCore().getModel("randInput");	
+
+			var oDate = new sap.ui.model.json.JSONModel({
+				date: date,
+				user: "user",
+				number: id,
+				type: typeInput,
+				state: state
+			}) ;
+			this.getView().setModel(oDate);
+		
+			this.getResourceBundle();
+			this.onUpdateState();
+		},
+		getResourceBundle: function() {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+	   },
+        onBack : function () {
+			var sPreviousHash = History.getInstance().getPreviousHash();
+
+			//The history contains a previous entry
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				// There is no history!
+				// replace the current hash with page 1 (will not add an history entry)
+				this.getOwnerComponent().getRouter().navTo("page1", null, true);
+			}
+		},
+
+		onExit: function(){
+			this.getOwnerComponent().getRouter().navTo("page1");
+		},
+		onCreate: function(){
+			
+		},
+		onCheck: function(){
+			var idClient = this.getView().byId("idClient");
+			
+			this.onUpdateState();
+			this._getDateResorces();
+		},
+		onUpdateState: function(){
+			var oBundle = this.getResourceBundle();
+			state = oBundle.getText("state01")
+		},
+		onAddRow: function(oEvent) {
+			var oItem = new sap.m.ColumnListItem({
+			cells : [ new sap.m.Input(), new sap.m.Input({showValueHelp : true}), new sap.m.Input(), new sap.m.Input(), new sap.m.Input(), new sap.m.Input(), new sap.m.Input(), new sap.m.Input() ]
+				//new sap.m.Input({showValueHelp : true}) for cell
+			});
+			
+			var oTable = this.getView().byId('idPositionTable');
+			oTable.addItem(oItem);
+		},
+
+		onDeleteRow: function(oEvent){
+			alert("i don't work ;D");
+			var oTable = this.getView().byId('idPositionTable');
+			oTable.removeItem(oEvent.getSource().getParent());
+		}
+    });
+});
