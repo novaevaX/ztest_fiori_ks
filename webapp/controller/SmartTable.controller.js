39e8d14b48733b257d0cd5fe9a4827dbfef220b7
rndrps <filipp.sitdikov@gmail.com>
2022-10-25 17:46:31 +0300

    SmartTableFix

--- webapp/controller/SmartTable.controller.js
+++ webapp/controller/SmartTable.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller"
   ], function(Controller) {
     "use strict";
-    return Controller.extend("sap.ui.demo.testAppUI5.controller.SmartTable", {
+    return Controller.extend("ztest_fiori_ks.controller.SmartTable", {
       onInit: function() {
         // this.getView().bindElement("/Documents");
       }
